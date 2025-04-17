df6b212ca9a552d00044cac7737925c8828d3b5f
kricsleo <kricsleo@163.com>
2025-04-17 14:12:28 +0800

    try 9

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,18 +4,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   nitro: {
     routeRules: {
+      '/api/foo/*/test': {
+        isr: true
+      }
         // All routes should be ISR
-        '/**': {
-            // isr: {
-            //   allowQuery: ["name", "url"],
-            //   passQuery: true,
-            // },
-            isr: {
-              expiration: 60,
-              // passQuery: true,
-              // allowQuery: ['name', 'url'],
-            }
-        },
+        // '/api/foo/*/test': {
+        //     // isr: {
+        //     //   allowQuery: ["name", "url"],
+        //     //   passQuery: true,
+        //     // },
+        //     isr: {
+        //       expiration: 60,
+        //       // passQuery: true,
+        //       // allowQuery: ['name', 'url'],
+        //     }
+        // },
       //   '/api/bar': {
       //     isr: {
       //       passQuery: true,
